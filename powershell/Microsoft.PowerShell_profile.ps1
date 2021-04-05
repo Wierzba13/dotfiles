e660f16318ce57184025a6b43a7ab512f0a94af1
Raul Wierzbinski <Wierzba@tuta.io>
2021-04-05 17:10:27 +0200

    Initial commit

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+Screenfetch
+# Set-PoshPrompt -Theme powerlevel10k_lean
+# Set-PoshPrompt -Theme powerline
+# Set-PoshPrompt -Theme slim
+
+
+# Set-ExecutionPolicy Bypass
